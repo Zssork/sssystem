e4c6920a1875bbaddb1d19f97a68240ff760de91
Zssork <readock@gmx.de>
2026-08-04 20:30:23 +0200

    convert config to lua

--- theme/.config/hypr/scripts/enshure-wal.sh
+++ theme/.config/hypr/scripts/enshure-wal.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-color_config_path="/home/$USER/.cache/wal/colors-hyprland.conf"
+# color_config_path="/home/$USER/.cache/wal/colors-hyprland.conf"
+color_config_path="/home/$USER/.cache/wal/colors-hyprland.lua"
 fallback_wallpaper="/home/$USER/.config/hypr/resources/ruan-jia.jpg"
 
 if [ ! -e "$color_config_path" ];then
